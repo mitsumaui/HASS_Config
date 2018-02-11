2e76f4a9263e0e58b9c50c7ff6ace4039dfede17
Jason Whitelock <jason@Jasons-MacBook-Pro.local>
2018-02-11 21:04:39 +0000

    Bug fixes

--- bin/update_config_from_github.sh
+++ bin/update_config_from_github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "/home/hass/.homeassistant/"
-git pull
+sudo git pull
--- bin/update_new_commits_sensor.sh
+++ bin/update_new_commits_sensor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "/home/hass/.homeassistant"
-git fetch
-commits="$(git rev-list --count master..origin/master)"
+sudo git fetch
+commits="$(sudo git rev-list --count master..origin/master)"
 
 curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://127.0.0.1:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}" -k
