82ed01d12343715782d6b22df4f19d379492ee90
Jason Whitelock <jason@Jasons-MacBook-Pro.local>
2018-02-11 22:29:37 +0000

    Cert renew overall with uPNP

---
+++ bin/cert_renew.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/home/hass/.homeassistant/bin/certbot_upnp.sh add
+~/certbot/certbot-auto renew --quiet --no-self-upgrade --standalone --preferred-challenges tls-sni-01 --tls-sni-01-port 8124
+/home/hass/.homeassistant/bin/certbot_upnp.sh remove
+
+sudo systemctl restart home-assistant@hass.service
---
+++ bin/certbot_upnp.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import miniupnpc
+u = miniupnpc.UPnP()
+u.discoverdelay=200;u.discover();u.selectigd()
+
+import sys
+if len(sys.argv) != 1:
+    sys.stderr.write("Syntax: certbot_upnp [add]/[remove]")
+    sys.exit(1)
+
+if sys.argv[1] == 'add':
+    u.addportmapping(443,'TCP',u.lanaddr,8124,'CertBot_Renew','')
+
+if sys.argv[1] == 'remove':
+    u.deleteportmapping(443,'TCP')
